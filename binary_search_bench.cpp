5e8c0d7742644df4bfe9cb41b5e9aa86673dd3e3
tdowns <travis.downs@gmail.com>
2019-05-03 19:48:28 -0500

    add SIMD tests, conditional on __AVX2__ to benchmark

--- binary_search_bench.cpp
+++ binary_search_bench.cpp
@@ -4,6 +4,9 @@
 #include "binary_search/plain_interleaving.hpp"
 #include "binary_search/group_prefetching.hpp"
 
+#ifdef __AVX2__
+#include "binary_search/simd.hpp"
+#endif
 
 BENCHMARK_TEMPLATE(BM_benchmark, stl_binary_search);
 BENCHMARK_TEMPLATE(BM_benchmark, stl_lower_bound);
@@ -17,5 +20,10 @@ BENCHMARK_TEMPLATE(BM_benchmark, plain_interleaving<8>);
 
 BENCHMARK_TEMPLATE(BM_benchmark, group_prefetching<4>);
 BENCHMARK_TEMPLATE(BM_benchmark, group_prefetching<8>);
+
+#ifdef __AVX2__
+BENCHMARK_TEMPLATE(BM_benchmark, simd_avx2);
+BENCHMARK_TEMPLATE(BM_benchmark, simd_avx2_interleaved2);
+#endif
 
 BENCHMARK_MAIN();
